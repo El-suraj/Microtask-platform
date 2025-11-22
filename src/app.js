c19126f61d18224041e72b6dc0ae6bcf7ff4e902
El-suraj <surajohussaini7@gmail.com>
2025-11-22 15:52:36 +0100

    added helmet to secure applicaton

--- src/app.js
+++ src/app.js
@@ -7,14 +7,23 @@ import taskRoutes from "./routes/task.routes.js";
 import walletRoutes from "./routes/wallet.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import submissionRoutes from './routes/submission.routes.js';
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 
 dotenv.config();
 
 const app = express();
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 0, // limit each IP to 100 requests per windowMs
+  message:
+    "Too many requests from this IP, please try again after a 15 minute break",
+});
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
-
+app.use('/api/auth', authLimiter);
 
 
 
