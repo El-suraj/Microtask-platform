b2c225bc1eb686dd22b474323463638848791508
El-suraj <surajohussaini7@gmail.com>
2025-12-03 16:48:16 +0100

    updated

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
-import { registerValidation, loginValidation, validate } from '../validators/authValidators.js';
+import { registerValidation, loginValidation, validate } from '../validators/authvalidators.js';
 
 const router = express.Router();
 
